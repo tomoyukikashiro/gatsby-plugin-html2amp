c8ce5723d467bc73c803831ac53bd425409626e2
tomoyukikashiro <ksrtmyk@gmail.com>
2018-11-17 23:24:25 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+
+# gatsby-plugin-html2amp
+
+Create AMP(Accelerated Mobile Pages) for your Gatsby site by using [html2amp](https://github.com/tomoyukikashiro/html2amp)
+
+## Install
+
+```bash
+$ npm install --save gatsby-plugin-html2amp
+```
+
+## How to use
+
+In gatsby-config.js
+```js
+{
+  resolve: 'gatsby-plugin-html2amp',
+  options: {
+    files: ['post/**/index.html', 'index.html'],
+    publicPath: 'public',
+    gaConfigPath: 'gaConfig.json',
+    dist: 'public/dist'
+  }
+}
+```
+
+## Options
+
+- files
+  - List of generated html path patterns from `publicPath`
+  - Default is `[**/*.html]`
+- publicPath
+  - Directory for building by gatsby
+  - Default is `public`
+- gaConfigPath
+  - amp-analytics config json for [google analytics](https://www.ampproject.org/docs/analytics/analytics-vendors)
+  - The path is from `publicPath`
+  - Optional
+- dist
+  - Path to output
+  - If the options is not set files are override by AMP result
---
+++ gatsby-node.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const fs = require('fs-extra')
+const globby = require('globby')
+const html2amp = require('html2amp')
+
+exports.onPostBuild = (_, pluginOptions) => {
+  const defaultOptions = {
+    files: ['**/*.html'],
+    publicPath: 'public',
+    gaConfigPath: null,
+    dist: null
+  }
+  const { files, publicPath, gaConfigPath, dist } = { ...defaultOptions, ...pluginOptions }
+  const absolutePaths = files.map(file => path.join(process.cwd(), publicPath, file))
+  const htmls = globby.sync(absolutePaths)
+  const config = { gaConfigPath, cwd: path.join(process.cwd(), publicPath) }
+  const promises = htmls.map(async html => {
+    const buffer = fs.readFileSync(html)
+    const amp = await html2amp(buffer.toString(), config)
+    if (dist) {
+      const newFilePath = html.replace(path.join(process.cwd(), publicPath), path.join(process.cwd(), dist))
+      fs.outputFileSync(newFilePath, amp)
+    } else {
+      fs.writeFileSync(html, amp)
+    }
+  })
+  return Promise.all(promises)
+}
