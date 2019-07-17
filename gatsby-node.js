c37e025f69ed1f9ba5a6a3859168e2836cf90b52
Andrew Schastlivcev <andrew.schastlivcev@sparkybit.com>
2019-07-17 11:37:15 +0300

    correct slash path for Windows platform

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,29 +1,45 @@
-const path = require('path')
-const fs = require('fs-extra')
-const globby = require('globby')
-const html2amp = require('html2amp')
+const path = require("path");
+const fs = require("fs-extra");
+const globby = require("globby");
+const html2amp = require("html2amp");
 
 exports.onPostBuild = (_, pluginOptions) => {
   const defaultOptions = {
-    files: ['**/*.html'],
-    publicPath: 'public',
+    files: ["**/*.html"],
+    publicPath: "public",
     gaConfigPath: null,
     dist: null,
     serviceWorker: null
-  }
-  const { files, publicPath, gaConfigPath, dist, serviceWorker } = { ...defaultOptions, ...pluginOptions }
-  const absolutePaths = files.map(file => path.join(process.cwd(), publicPath, file))
-  const htmls = globby.sync(absolutePaths)
-  const config = { gaConfigPath, cwd: path.join(process.cwd(), publicPath), serviceWorker }
+  };
+  const { files, publicPath, gaConfigPath, dist, serviceWorker } = {
+    ...defaultOptions,
+    ...pluginOptions
+  };
+  const absolutePaths = files.map(file =>
+    path.join(process.cwd(), publicPath, file)
+  );
+  const htmls = globby.sync(absolutePaths);
+  const config = {
+    gaConfigPath,
+    cwd: path.join(process.cwd(), publicPath),
+    serviceWorker
+  };
   const promises = htmls.map(async html => {
-    const buffer = fs.readFileSync(html)
-    const amp = await html2amp(buffer.toString(), config)
+    const buffer = fs.readFileSync(html);
+    const winPlatform = process.platform === "win32"; //check win32 platform
+    const amp = await html2amp(buffer.toString(), config);
     if (dist) {
-      const newFilePath = html.replace(path.join(process.cwd(), publicPath), path.join(process.cwd(), dist))
-      fs.outputFileSync(newFilePath, amp)
+      const basePath = winPlatform
+        ? slash(path.join(process.cwd(), publicPath)) //if win32 replace slash
+        : path.join(process.cwd(), publicPath);
+      const ampPath = winPlatform
+        ? slash(path.join(process.cwd(), dist)) //if win32 replace slash
+        : path.join(process.cwd(), dist);
+      const newFilePath = html.replace(basePath, ampPath);
+      fs.outputFileSync(newFilePath, amp);
     } else {
-      fs.writeFileSync(html, amp)
+      fs.writeFileSync(html, amp);
     }
-  })
-  return Promise.all(promises)
-}
+  });
+  return Promise.all(promises);
+};
